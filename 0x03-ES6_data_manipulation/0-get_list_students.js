c453d2b601269269c4dae54a09b24d857ac2e4c6
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-07-02 08:45:41 +0000

    first task

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListStudents(){
+
+	const p1 = {id:1 , firstName:"Guillaume", location:"San Francisco"};
+	const p2 = {id:2 , firstName:"James", location:"Columbia"};
+	const p3 = {id:3 , firstName:"Serena", location:"San Francisco"};
+
+	const people = [p1, p2 ,p3];
+
+	return people;
+}
